4c4f3937d8f0d0a139ccb85fcd7b5c3b597fe12f
Kakajann Dortguly <im@kakajann.com>
2025-01-17 21:58:37 +0500

    feat: add bg blur to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Avatar from "@/components/Avatar";
+import BackgroundBlur from "@/components/BackgroundBlur";
 
 const Home = () => {
   return (
-    <div className="container pt-20 flex flex-col gap-20 pb-20">
-      <Avatar />
-    </div>
+    <BackgroundBlur>
+      <div className="container pt-20 flex flex-col gap-20 pb-20">
+        <Avatar />
+      </div>
+    </BackgroundBlur>
   );
 };
 
