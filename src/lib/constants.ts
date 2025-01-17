cb30ba8bf706371b6a6bc8a189770501b26ca66b
Kakajann Dortguly <im@kakajann.com>
2025-01-17 20:48:18 +0500

    feat: add app links

---
+++ src/lib/constants.ts
@@ -0,0 +1,20 @@
+const appLinks = [
+  {
+    name: "Home",
+    path: "/home",
+  },
+  {
+    name: "Meet",
+    path: "/meet",
+  },
+  {
+    name: "Work",
+    path: "/work",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+export { appLinks };
